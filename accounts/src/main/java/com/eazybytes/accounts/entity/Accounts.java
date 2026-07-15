3b9ed86a402ae110706dff282eab09c8363c40f3
Lowe Kumar <lowekr00@gmail.com>
2026-07-15 06:27:55 +0530

    Created Customer then its account and GlobalExceptionHandler usecase

--- accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
+++ accounts/src/main/java/com/eazybytes/accounts/entity/Accounts.java
@@ -15,13 +15,16 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Accounts extends BaseEntity{
 
-    @Id
+    @Column(name="customer_id")
+    private Long customerId;
+
     @Column(name="account_number")
-    private String accountNumber;
+    @Id
+    private Long accountNumber;
+
     @Column(name="account_type")
     private String accountType;
+
     @Column(name="branch_address")
     private String branchAddress;
-    @Column(name="customer_id")
-    private String customerId;
 }
--- accounts/src/main/java/com/eazybytes/accounts/entity/Customer.java
+++ accounts/src/main/java/com/eazybytes/accounts/entity/Customer.java
@@ -16,7 +16,7 @@ public class Customer extends BaseEntity{
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO, generator="native")
     @Column(name="customer_id")
-    private String customerId;
+    private Long customerId;
     private String name;
     private String email;
     @Column(name = "mobile_number")
