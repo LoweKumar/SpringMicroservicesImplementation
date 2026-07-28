68b424aa96089e6ff5f89c3fcd79e47449009b5f
Lowe Kumar <lowekr00@gmail.com>
2026-07-28 19:29:45 +0530

    Lecture 21 create update API inside accounts microservice

--- accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -3,6 +3,7 @@ package com.eazybytes.accounts.controller;
 import com.eazybytes.accounts.constants.AccountsConstants;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
+import com.eazybytes.accounts.entity.Accounts;
 import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.service.IAccountsService;
 import jakarta.validation.constraints.Pattern;
@@ -36,5 +37,19 @@ public class AccountsController {
                                                            String mobileNumber) {
         CustomerDto customerDto = iAccountsService.fetchAccount(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccount(@RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated){
+            return ResponseEntity.status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }
+        else
+        {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDto(AccountsConstants.STATUS_500, AccountsConstants.MESSAGE_500));
+        }
     }
 }
